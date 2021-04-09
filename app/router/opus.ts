9fa9244c45c102a0425a09bab7cc3373e12c325a
lemonomg <myx0930@163.com>
2021-04-09 13:18:27 +0800

    修改路由

--- app/router/opus.ts
+++ app/router/opus.ts
@@ -3,5 +3,6 @@ import { Application } from 'egg'
 module.exports = (app: Application) => {
     const { router, controller } = app
 
+    router.get('/api/opus/',controller.opus.getData)
     router.get('/api/opus/:id', controller.opus.getData)
 }
